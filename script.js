aa109c90c476d9091b8fe0417cb13cb57e4f3202
Eugen Burkenia <burikievgen@gmail.com>
2021-12-05 20:44:01 -0500

    lesson06

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-console.log('dasfsdf');
+//lesson06
+//guess a Number from 1 to 100
+
+
+const myNumber = Math.floor(Math.random() * 100) + 1;
+let guess;
+let attempts = 6;
+let msg = "Guess a number from 1 to 100";
+
+const isNumber = function(num) {
+    return !isNaN(parseFloat(num)) && isFinite(num);
+}
+
+const gameOver = () => {
+    alert("Game Over")
+}
+
+const asking = (guess) => {
+      
+    guess = prompt(msg);
+    
+    if(guess == null) {
+        return gameOver();
+    }
+    
+    const game = (guess) => {
+        if (guess == myNumber) {
+            alert('YES! That was my number');
+        } else if (guess > myNumber) {
+            msg = 'No. The number is less';
+            asking(msg);
+        } else if (guess < myNumber) {
+            msg = 'No. My number is bigger';
+            asking(msg);
+        } else {
+            msg = 'Please input a NUMBER';
+            asking(msg);
+        }
+    }
+    game(guess)
+}
+
+asking(guess);
