ec6a3c529ee210e121d6086847ae30295c73c534
Eugen Burkenia <burikievgen@gmail.com>
2021-12-07 22:25:13 -0500

    used closures

--- script.js
+++ script.js
@@ -1,43 +1,30 @@
 //lesson06
 //guess a Number from 1 to 100
 
+const game = () => { 
+    const myNumber = Math.floor(Math.random() * 100) + 1;
 
-const myNumber = Math.floor(Math.random() * 100) + 1;
-let guess;
-let attempts = 6;
-let msg = "Guess a number from 1 to 100";
-
-const isNumber = function(num) {
-    return !isNaN(parseFloat(num)) && isFinite(num);
-}
-
-const gameOver = () => {
-    alert("Game Over")
-}
-
-const asking = (guess) => {
-      
-    guess = prompt(msg);
-    
-    if(guess == null) {
-        return gameOver();
-    }
-    
-    const game = (guess) => {
-        if (guess == myNumber) {
+    const asking = (msg) => {
+        msg = msg || "Guess a number from 1 to 100";
+        const guess = prompt(msg);
+        
+        if(guess === null) {
+            alert ('Game Over');
+            return;
+        } else if (+guess === myNumber) {
             alert('YES! That was my number');
-        } else if (guess > myNumber) {
+        } else if (+guess > myNumber) {
             msg = 'No. The number is less';
             asking(msg);
-        } else if (guess < myNumber) {
+        } else if (+guess < myNumber) {            
             msg = 'No. My number is bigger';
             asking(msg);
         } else {
             msg = 'Please input a NUMBER';
             asking(msg);
         }
-    }
-    game(guess)
+}   
+    return asking;
 }
+game()();
 
-asking(guess);
